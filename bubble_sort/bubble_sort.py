06e87629d3abd4711654d7b53a5b0587c812d0ee
Chitsaka <chitsaka.gari@strathmore.edu>
2025-06-20 11:46:53 +0300

    bubble and selection sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
 def bubble_sort(unsorted_list):
+
+    for i in range(len(unsorted_list)):
+
+        for u in range(0,len(unsorted_list)-i-1):
+            if unsorted_list[u] > unsorted_list[u+1]:
+
+                unsorted_list[u],unsorted_list[u+1]=unsorted_list[u+1],unsorted_list[u]
+
     # TODO: Implement bubble sort
-    pass
+    return unsorted_list
+
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,15 @@
 def selection_sort(arr):
+    s = len(arr)
+
+    for i in range(s):
+
+        minimum_number =i
+        for n in range(i+1,s):
+            if arr[n]<arr[minimum_number]:
+                minimum_number= n
+
+                arr[i], arr[minimum_number]= arr[minimum_number], arr[i]
+
     # TODO: Implement selection sort
-    pass
+    return arr
+
